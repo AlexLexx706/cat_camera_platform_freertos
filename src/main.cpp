2e7a764de27818b9dd4fe5e9d1fcb2579411295a
alex <akalmykov@topcon.com>
2024-06-12 01:30:16 +0200

    encoder added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "FreeRTOS.h"
 #include "ICM42688/ICM42688.h"
+#include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "semphr.h"
@@ -7,8 +8,15 @@
 #include <math.h>
 #include <stdio.h>
 
+#define ENC_A1 8
+#define ENC_B1 9
+
+#define ENC_A2 10
+#define ENC_B2 11
+
 #define ICM42688_IRQ_PIN 22
 #define GYRO_BIAS_TAU 20.f
+
 volatile static uint32_t packet_time;
 volatile static uint32_t last_packet_time = 0;
 volatile static uint32_t dt = 0;
@@ -18,7 +26,17 @@ volatile static float gyro_bias[3] = {0.f};
 // static float gyro_scale[3] = {1.f, 1.f, 1.0257511014952223f};
 static float gyro_scale[3] = {1.f, 1.f, 1.f};
 
-SemaphoreHandle_t imu_data_semaphore;
+typedef struct EncoderState {
+    int8_t last_encoded;
+    int32_t encoder_value;
+} EncoderState;
+
+static EncoderState enc_1 = {};
+static EncoderState enc_2 = {};
+
+static SemaphoreHandle_t imu_data_semaphore;
+static SemaphoreHandle_t encoder_semaphore;
+volatile static bool is_enc1;
 static ICM42688 IMU;
 
 static void gpio_callback(uint gpio, uint32_t events) {
@@ -28,14 +46,42 @@ static void gpio_callback(uint gpio, uint32_t events) {
     if (gpio == ICM42688_IRQ_PIN) {
         packet_time = time_us_32();
         xSemaphoreGiveFromISR(imu_data_semaphore, &xHigherPriorityTaskWoken);
+        // process encoder
+    } else {
+        is_enc1 = (gpio == ENC_A1 || gpio == ENC_B1);
+        xSemaphoreGiveFromISR(encoder_semaphore, &xHigherPriorityTaskWoken);
     }
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+static void process_encoder(void *) {
+    for (;;) {
+        xSemaphoreTake(encoder_semaphore, portMAX_DELAY);
+
+        EncoderState *state;
+        int encoded;
+        if (is_enc1) {
+            state = &enc_1;
+            encoded = (gpio_get(ENC_A1) << 1) | gpio_get(ENC_B1);
+        } else {
+            state = &enc_2;
+            encoded = (gpio_get(ENC_A2) << 1) | gpio_get(ENC_B2);
+        }
+        int sum = (state->last_encoded << 2) | encoded;
+
+        if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
+            state->encoder_value += 1;
+        if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
+            state->encoder_value -= 1;
+        state->last_encoded = encoded;
+    }
 }
 
 static void print_state(void *) {
     while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000 / 10));
-        printf("%+6.5f %+6.3f %+6.3f\n", gyro_bias[2], gyro_rate[2], angles[2]);
+        printf("%+6.5f %+6.3f %+6.3f %d %d\n", gyro_bias[2], gyro_rate[2],
+               angles[2], enc_1.encoder_value, enc_2.encoder_value);
     }
 }
 
@@ -81,8 +127,42 @@ int main() {
     stdio_init_all();
     sleep_ms(2000);
     imu_data_semaphore = xSemaphoreCreateBinary();
+    encoder_semaphore = xSemaphoreCreateBinary();
 
-    if (imu_data_semaphore != NULL) {
+    if (imu_data_semaphore != NULL && encoder_semaphore != NULL) {
+        // init encoder
+        gpio_init(ENC_A1);
+        gpio_init(ENC_A2);
+        gpio_init(ENC_B1);
+        gpio_init(ENC_B2);
+
+        gpio_set_dir(ENC_A1, GPIO_IN);
+        gpio_set_dir(ENC_A2, GPIO_IN);
+        gpio_set_dir(ENC_B1, GPIO_IN);
+        gpio_set_dir(ENC_B2, GPIO_IN);
+
+        // https://cdn-shop.adafruit.com/product-files/4640/n20+motors_C15011+6V.pdf
+        gpio_pull_up(ENC_A1);
+        gpio_pull_up(ENC_A2);
+        gpio_pull_up(ENC_B1);
+        gpio_pull_up(ENC_B2);
+
+        gpio_set_irq_enabled_with_callback(
+            ENC_A1, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true,
+            &gpio_callback);
+
+        gpio_set_irq_enabled_with_callback(
+            ENC_B1, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true,
+            &gpio_callback);
+
+        gpio_set_irq_enabled_with_callback(
+            ENC_A2, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true,
+            &gpio_callback);
+
+        gpio_set_irq_enabled_with_callback(
+            ENC_B2, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true,
+            &gpio_callback);
+
         // start communication with IMU
         int status = IMU.begin();
         gpio_set_irq_enabled_with_callback(ICM42688_IRQ_PIN, GPIO_IRQ_EDGE_RISE,
@@ -101,8 +181,9 @@ int main() {
         IMU.setAccelFS(ICM42688::gpm2);
         IMU.enableDataReadyInterrupt();
 
-        xTaskCreate(process_imu, "imu Task", 1024, NULL, 2, NULL);
-        xTaskCreate(print_state, "test_task", 1024, NULL, 1, NULL);
+        xTaskCreate(process_imu, "imu", 1024, NULL, 3, NULL);
+        xTaskCreate(process_encoder, "encoder", 1024, NULL, 2, NULL);
+        xTaskCreate(print_state, "print", 1024, NULL, 1, NULL);
 
         // enabling the data ready interrupt
         vTaskStartScheduler();
